cfb8873f897b4aa51c4c1b0192453edbb03400fd
PhilippeRobitaille <157495484+PhilippeRobitaille@users.noreply.github.com>
2024-02-13 10:09:02 -0500

    Fonction tourdragon

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -20,30 +20,42 @@ namespace PO1_Encapsulation
         private void ActualiserLabels()
         {
             lbl_pointsVieJoueur.Text = " Vie du joueur : " + joueur.PointDeVie.ToString();
+
             lbl_pointsVieDragon.Text = " Vie du dragon : " + dragon.PointDeVie.ToString();
+
             lbl_puissanceAttaqueJoueur.Text = " Attaque du joueur " + joueur.PointsDePuissance.ToString();
+
             lbl_puissanceAttaqueDragon.Text = " Attaque du dragon " + dragon.PointsDePuissance.ToString();
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
             dragon.PointDeVie -= joueur.PointsDePuissance;
+            TourDragon();
             ActualiserLabels();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
+            joueur.PointDeVie += 5;
+            TourDragon();
             ActualiserLabels();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            joueur.PointsDePuissance++;
+            TourDragon();
             ActualiserLabels();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ActualiserLabels();
+        }
+        private void TourDragon()
+        {
+            joueur.PointDeVie -= dragon.PointsDePuissance;
         }
     }
 }
